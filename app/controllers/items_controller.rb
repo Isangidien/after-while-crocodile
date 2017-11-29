b68d7cfb52004119db5a1f8fcf58ce231cf95a50
isangidien <i.inessa.miller@gmail.com>
2017-11-29 16:36:08 -0500

    error for my partial items

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,31 @@
+class ItemsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @item = Item.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @items = @user.items
+    @item = current_user.items.build(item_params)
+    @item.user = @user
+    @new_item = Item.new
+
+    if @item.save
+      flash[:notice] = "Your item was saved."
+    else
+      flash[:error] = "There was an error saving your item."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :body)
+  end
+end
